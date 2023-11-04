e4b7618c2fe4943fc97a70be0ed5cd59eab30162
zafir90 <137960910+zafir90@users.noreply.github.com>
2023-11-04 19:36:26 -0300

    Add files via upload

---
+++ js/calculadora.js
@@ -0,0 +1,16 @@
+function calcNumbers(result){
+    
+    form.displayResult.value = form.displayResult.value + result;
+
+    
+  }
+
+  function resultado(){
+
+    form.displayResult.value = eval(form.displayResult.value);
+  }
+
+  function limpiar(){
+
+    form.displayResult.value = "";
+  }
